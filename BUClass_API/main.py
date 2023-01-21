2c106a07d8e971d3a921641b8f2ed04f694f9b39
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-01-21 14:49:17 -0500

    Methods[Get] added

--- BUClass_API/main.py
+++ BUClass_API/main.py
@@ -1,21 +1,12 @@
-import datetime
-
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
-
-    return render_template('index.html', times=dummy_times)
-
+@app.route('/', methods=['GET'])
+def out():
+    command = request.args.get('command')
+    return command
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
