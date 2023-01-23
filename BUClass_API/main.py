0ab6bdfa8090b9a1d537e1d2133a1b866d263bf3
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-01-23 12:12:55 -0500

    new filters

--- BUClass_API/main.py
+++ BUClass_API/main.py
@@ -4,22 +4,41 @@ import sqlite3
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def out():
-    req = request.args.get('search')
-    con = sqlite3.connect( 'class_storage.db' )
+@app.route('/search', methods=['GET'])
+def search():
+    req = request.args.get('code')
+    con = sqlite3.connect('BUClass_API/class_storage.db')
     cur = con.cursor()
-    
-    info = cur.execute(f'SELECT * FROM ClassData WHERE Name = "{req}"')
-    
-    return info.fetchall()
+
+    info = cur.execute(f'SELECT * \
+                        FROM ClassData \
+                        WHERE Name = "{req}"')
+
+    clean_info = [i for i in info.fetchall()[0]]
+    hub_credit = clean_info[-1].split("'")
+    clean_hub_credit = [i for i in hub_credit if i[0].isalpha()]
+    clean_info = clean_info[:-1] + [clean_hub_credit]
+    return f'{clean_info}'
+
+
+@app.route('/filter', methods=['GET'])
+def filter():
+    con = sqlite3.connect('BUClass_API/class_storage.db')
+    cur = con.cursor()
+    info = ''
+
+    req = request.args.get('prereq')
+    if req:
+        info = cur.execute(f'SELECT Name \
+                            FROM ClassData \
+                            WHERE Prereq like "%{req}%"')
+
+    if info:
+        return f'{[i[0] for i in info.fetchall()]}'
+    else:
+        return 'No results found.'
+
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
+    # Local testing
     app.run(host='127.0.0.1', port=8080, debug=True)
